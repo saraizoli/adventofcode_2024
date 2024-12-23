883514982312f405f7e4ae41f8d2dce9f59f9032
saraizoli <zoltanisarai@gmail.com>
2024-12-23 19:11:48 +0100

    Day23

--- src/main/application/Main.java
+++ src/main/application/Main.java
@@ -1,12 +1,12 @@
 package main.application;
 
-import main.day22.Day22;
+import main.day23.Day23;
 import main.utils.Day;
 
 public class Main {
     public static void main(String[] args) {
 
-        Day<?> day = new Day22();
+        Day<?> day = new Day23();
         day.printSolution1WithTime();
         day.printSolution2WithTime();
 
---
+++ src/main/day23/Day23.java
@@ -0,0 +1,73 @@
+package main.day23;
+
+import main.utils.Day;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day23 extends Day<Integer> {
+    private final Map<String, Set<String>> edges;
+    private final Set<Set<String>> base;
+
+    public Day23() {
+//        List<String> raw = getReader().readAsStringList("day23_sample.txt");
+        List<String> raw = getReader().readAsStringList(23);
+        edges = new HashMap<>();
+        base = new HashSet<>();
+        for (String s : raw) {
+            String[] t = s.split("-");
+            base.add(Set.of(t[0], t[1]));
+            edges.computeIfAbsent(t[0], k -> new HashSet<>()).add(t[1]);
+            edges.computeIfAbsent(t[1], k -> new HashSet<>()).add(t[0]);
+        }
+    }
+
+    @Override
+    public Integer getSolution1() {
+        Set<Set<String>> cliqs = new HashSet<>();
+        edges.forEach((bp, neighbs) -> {
+            if (bp.charAt(0) != 't') {
+                return;
+            }
+            for (String n : neighbs) {
+                HashSet<String> base = new HashSet<>(neighbs);
+
+                base.retainAll(edges.get(n));
+                base.forEach(p -> cliqs.add(Set.of(bp, n, p)));
+            }
+        });
+        return cliqs.size();
+    }
+
+    private Set<Set<String>> nextSizeCliq(Set<Set<String>> prev) {
+        Set<Set<String>> next = new HashSet<>();
+        for (Set<String> existingCliq : prev) {
+            Set<String> commonEdges = null;
+            for (String cliqMember : existingCliq) {
+                if (commonEdges == null) {
+                    commonEdges = new HashSet<>(edges.get(cliqMember));
+                } else {
+                    commonEdges.retainAll(edges.get(cliqMember));
+                }
+            }
+            commonEdges.forEach(e -> {
+                Set<String> toAdd = new HashSet<>(existingCliq);
+                toAdd.add(e);
+                next.add(toAdd);
+            });
+        }
+        return next;
+    }
+
+
+    @Override
+    public Integer getSolution2() {
+        Set<Set<String>> curr = base;
+        while (curr.size() > 1) {
+            curr = nextSizeCliq(curr);
+        }
+        String res = curr.stream().findFirst().orElseThrow().stream().sorted().collect(Collectors.joining(","));
+        System.out.println(res);
+        return 0;
+    }
+}
